a22cb612367569d31999fff39fd3989c61e40384
lheesoo1020 <lheesoo1020@gmail.com>
2024-12-02 16:52:49 +0900

    chore: securityconfig 추가

---
+++ src/main/java/kr/heesoo/hsarchive/common/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package kr.heesoo.hsarchive.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/api/login", "/api/signup").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
