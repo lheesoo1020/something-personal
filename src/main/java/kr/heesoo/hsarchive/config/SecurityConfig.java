f4d6de9d718eeb812172443b626ab13df9916b59
lheesoo1020 <lheesoo1020@gmail.com>
2024-12-04 14:04:36 +0900

    chore: 기본 폼로그인 사용

--- src/main/java/kr/heesoo/hsarchive/config/SecurityConfig.java
+++ src/main/java/kr/heesoo/hsarchive/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package kr.heesoo.hsarchive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,9 +19,10 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/member/login", "/member/signup").permitAll()
+                        .requestMatchers("/auth/login", "/auth/signup", "/login").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
